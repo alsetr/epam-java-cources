952dfe0802014e0c67b2190192ad9d913a320f5e
Aleksandr Trubin <alsetr@mail.ru>
2020-08-23 15:58:18 +0300

    modified:   src/main/java/com/epam/university/java/core/task001/Task001Impl.java 	new file:   src/main/java/com/epam/university/java/core/task002/Task002Impl.java

--- src/main/java/com/epam/university/java/core/task001/Task001Impl.java
+++ src/main/java/com/epam/university/java/core/task001/Task001Impl.java
@@ -1,23 +1,40 @@
 package com.epam.university.java.core.task001;
 
 public class Task001Impl implements Task001 {
+    private static double f;
+    private static double s;
+
     @Override
     public double addition(String firstNumber, String secondNumber) {
-        return 0;
+        checkArgument(firstNumber, secondNumber);
+        return f + s;
     }
 
     @Override
     public double subtraction(String firstNumber, String secondNumber) {
-        return 0;
+        checkArgument(firstNumber, secondNumber);
+        return f - s;
     }
 
     @Override
     public double multiplication(String firstNumber, String secondNumber) {
-        return 0;
+        checkArgument(firstNumber, secondNumber);
+        return f * s;
     }
 
     @Override
     public double division(String firstNumber, String secondNumber) {
-        return 0;
+        checkArgument(firstNumber, secondNumber);
+        return f / s;
+    }
+
+    private void checkArgument(String firstNumber, String secondNumber) {
+        if (firstNumber == null || secondNumber == null) {
+            throw new IllegalArgumentException();
+        }
+        f = Double.parseDouble(firstNumber);
+        s = Double.parseDouble(secondNumber);
     }
 }
+
+
