a89da7a92d2a7d12de4555df0bfe52c218ad15ec
Aleksandr Trubin <alsetr@mail.ru>
2020-09-20 22:33:13 +0300

    task 037 solved

--- src/main/java/com/epam/university/java/core/task031/ServerImpl.java
+++ src/main/java/com/epam/university/java/core/task031/ServerImpl.java
@@ -1,18 +1,42 @@
 package com.epam.university.java.core.task031;
 
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Scanner;
+
 public class ServerImpl implements Server {
+    Deque<String> messages = new ArrayDeque<>();
+    boolean isRunning;
+
     @Override
     public String readMessage() {
-        return null;
+        DataInputStream in = new DataInputStream(System.in);
+        String s = "";
+        try {
+            s = in.readUTF();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+//        Scanner scanner = new Scanner(System.in);
+//        StringBuilder sb = new StringBuilder();
+//        while (scanner.hasNextLine()) {
+//            sb.append(scanner.nextLine());
+//        }
+        return s;
     }
 
     @Override
     public void start() {
+        isRunning = true;
 
     }
 
     @Override
     public void stop() {
+        isRunning = false;
 
     }
 }
