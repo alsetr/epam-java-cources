2040e3022c8e672f35f45f59922a1efcc1c69294
Aleksandr Trubin <alsetr@mail.ru>
2020-11-04 18:02:37 +0300

    more tasks solved

--- src/main/java/com/epam/university/java/project/core/state/machine/domain/StateMachineStateImpl.java
+++ src/main/java/com/epam/university/java/project/core/state/machine/domain/StateMachineStateImpl.java
@@ -7,6 +7,7 @@ import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlRootElement;
+import java.util.Objects;
 
 @XmlRootElement(name = "transition")
 @XmlAccessorType(XmlAccessType.FIELD)
